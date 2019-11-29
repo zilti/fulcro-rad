077807382d78c58861163e2cf162be8dbabdbff3
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-11-29 07:09:29 +0100

    Format client

--- src/example/com/example/client.cljs
+++ src/example/com/example/client.cljs
@@ -1,40 +1,42 @@
 (ns com.example.client
   (:require
-    [com.example.ui :as ui :refer [Root]]
-    [com.example.ui.login-dialog :refer [LoginForm]]
-    [com.fulcrologic.fulcro.algorithms.form-state :as fs]
-    [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.networking.http-remote :as http]
-    [com.fulcrologic.rad.controller :as controller]
-    [com.fulcrologic.rad.schema :as schema]
-    [com.example.schema :refer [latest-schema]]
-    [com.fulcrologic.rad.authorization :as auth]
-    [com.fulcrologic.fulcro.ui-state-machines :as uism]
-    [com.fulcrologic.fulcro.components :as comp]
-    [com.fulcrologic.fulcro.data-fetch :as df]
-    [taoensso.timbre :as log]))
+   [com.example.ui :as ui :refer [Root]]
+   [com.example.ui.login-dialog :refer [LoginForm]]
+   [com.fulcrologic.fulcro.algorithms.form-state :as fs]
+   [com.fulcrologic.fulcro.application :as app]
+   [com.fulcrologic.fulcro.networking.http-remote :as http]
+   [com.fulcrologic.rad.controller :as controller]
+   [com.fulcrologic.rad.schema :as schema]
+   [com.example.schema :refer [latest-schema]]
+   [com.fulcrologic.rad.authorization :as auth]
+   [com.fulcrologic.fulcro.ui-state-machines :as uism]
+   [com.fulcrologic.fulcro.components :as comp]
+   [com.fulcrologic.fulcro.data-fetch :as df]))
 
-;; TODO: Constructor function. Allow option to completely autogenerate forms if desired.
+(defn- eql-transform [ast]
+  (df/elide-ast-nodes
+   ast (fn [k]
+         (or
+          (= k '[:com.fulcrologic.fulcro.ui-state-machines/asm-id _])
+          (= k df/marker-table)
+          (= k ::fs/config)
+          (= "ui" (when (keyword? k) (namespace k))ns)))))
 
-(defonce app (app/fulcro-app {:remotes              {:remote (http/fulcro-http-remote {})}
-
-                              :global-eql-transform (fn [ast]
-                                                      (let [kw-namespace (fn [k] (and (keyword? k) (namespace k)))]
-                                                        (df/elide-ast-nodes ast (fn [k]
-                                                                                  (let [ns (some-> k kw-namespace)]
-                                                                                    (or
-                                                                                      (= k '[:com.fulcrologic.fulcro.ui-state-machines/asm-id _])
-                                                                                      (= k df/marker-table)
-                                                                                      (= k ::fs/config)
-                                                                                      (and
-                                                                                        (string? ns)
-                                                                                        (= "ui" ns))))))))
-                              :client-did-mount     (fn [app]
-                                                      (auth/start! app {:local (uism/with-actor-class (comp/get-ident LoginForm {}) LoginForm)})
-                                                      (controller/start! app
-                                                        {::schema/schema        latest-schema
-                                                         ::controller/home-page ["landing-page"]
-                                                         ::controller/router    ui/MainRouter
-                                                         ::controller/id        :main-controller}))}))
+;; TODO: Constructor function. Allow option to completely autogenerate
+;; forms if desired.
+(defonce app
+  (app/fulcro-app
+   {:remotes {:remote (http/fulcro-http-remote {})}
+    :global-eql-transform #'eql-transform
+    :client-did-mount
+    (fn [app]
+      (auth/start! app {:local (-> LoginForm
+                                   (comp/get-ident {})
+                                   (uism/with-actor-class LoginForm))})
+      (controller/start! app
+                         {::schema/schema        latest-schema
+                          ::controller/home-page ["landing-page"]
+                          ::controller/router    ui/MainRouter
+                          ::controller/id        :main-controller}))}))
 
 (defn start [] (app/mount! app Root "app"))
